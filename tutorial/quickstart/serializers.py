f49f233de43ec1bb2332c9486cb71b8eecb3d472
Timo Stollenwerk <tisto@plone.org>
2015-08-28 16:18:51 +0200

    Remove operation from UserSerializer.

--- tutorial/quickstart/serializers.py
+++ tutorial/quickstart/serializers.py
@@ -4,8 +4,6 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 
-    operation = serializers.SerializerMethodField()
-
     class Meta:
         model = User
         fields = (
@@ -13,30 +11,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
             'username',
             'email',
             'groups',
-            'operation'
         )
-
-    def get_operation(self, obj):
-        return [
-            {
-                '@type': 'CreateResourceOperation',
-                'method': 'POST',
-                'title': 'Creates a new user',
-                'expects': '',
-                'returns': ''
-            },
-            {
-                '@type': 'ReplaceResourceOperation',
-                'method': 'PUT',
-                'title': 'Updates an existing user'
-            },
-            {
-                '@type': 'DeleteResourceOperation',
-                'method': 'DELETE',
-                'title': 'Removes an existing user'
-            },
-
-        ]
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
