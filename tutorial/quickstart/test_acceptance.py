9ae773f6fafd4a38b08aee6a172f8bd2b41ff23e
Timo Stollenwerk <tisto@plone.org>
2015-09-04 16:34:26 +0200

    Add detail view acceptance test.

--- tutorial/quickstart/test_acceptance.py
+++ tutorial/quickstart/test_acceptance.py
@@ -4,19 +4,43 @@ from tutorial.quickstart.models import Application
 
 
 @pytest.mark.django_db()
-def test_application_json_view(admin_client):
+def test_application_json_list_view(admin_client):
     app = Application()
     app.title = u'My first application'
     app.firstname = u'John'
     app.lastname = u'Doe'
     app.email = u'john@doe.com'
     app.save()
+
     response = admin_client.get(
         '/application/',
         HTTP_CONTENT_TYPE='application/json'
     )
+
     assert 200 == response.status_code
     assert 'application/json' in response.get('Content-Type')
+    assert u'PagedCollection' == json.loads(response.content).get('@type')
     assert 1 == len(json.loads(response.content).get('member'))
     assert u'My first application' == \
         json.loads(response.content).get('member')[0].get('title')
+
+
+@pytest.mark.django_db()
+def test_application_json_detail_view(admin_client):
+    app = Application()
+    app.title = u'My first application'
+    app.firstname = u'John'
+    app.lastname = u'Doe'
+    app.email = u'john@doe.com'
+    app.save()
+
+    response = admin_client.get(
+        '/application/{}/'.format(app.id),
+        HTTP_CONTENT_TYPE='application/json'
+    )
+
+    assert 200 == response.status_code
+    assert 'application/json' in response.get('Content-Type')
+    assert u'Resource' == json.loads(response.content).get('@type')
+    assert u'My first application' == \
+        json.loads(response.content).get('title')
