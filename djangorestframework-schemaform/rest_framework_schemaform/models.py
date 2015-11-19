dc8258a0360e8d29413902b5a07259140a45cd8c
Timo Stollenwerk <tisto@plone.org>
2015-11-19 13:13:32 +0100

    Add djangorestframework-schemaform.

---
+++ djangorestframework-schemaform/rest_framework_schemaform/models.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+
+class Application(models.Model):
+
+    title = models.CharField(
+        u'Title',
+        help_text=u'Title of the application',
+        max_length=255
+    )
+
+    description = models.TextField(
+        u'Description',
+        help_text=u'Description of the application',
+        null=True,
+        blank=True
+    )
+
+    salutation = models.CharField(
+        u'Salutation',
+        null=True,
+        blank=True,
+        max_length=255,
+        choices=(
+            ('mr', 'Mr'),
+            ('mrs', 'Mrs'),
+            ('ms', 'Ms'),
+        )
+    )
+
+    username = models.CharField(
+        u'Username',
+        help_text=u'External validation, anything but "Bob" is valid.',
+        null=True,
+        max_length=255
+    )
+
+    firstname = models.CharField(
+        u'First name',
+        help_text=u'Your first name',
+        max_length=255
+    )
+
+    lastname = models.CharField(
+        u'Last name',
+        help_text=u'Your last name',
+        max_length=255
+    )
+
+    street = models.CharField(
+        u'Street',
+        help_text=u'Your street name',
+        null=True,
+        max_length=255
+    )
+
+    house_number = models.CharField(
+        u'House number',
+        null=True,
+        max_length=255
+    )
+
+    zip_number = models.CharField(
+        u'ZIP number',
+        null=True,
+        max_length=5
+    )
+
+    city = models.CharField(
+        u'City',
+        null=True,
+        max_length=255
+    )
+
+    country = models.CharField(
+        u'Country',
+        null=True,
+        max_length=255
+    )
+
+    email = models.EmailField(
+        u'Email address',
+        help_text=u'Your email address',
+        null=True,
+        blank=True
+    )
+
+    age = models.IntegerField(
+        u'Age',
+        help_text='Your age',
+        null=True,
+        blank=True
+    )
+
+    date = models.DateField(
+        u'Date',
+        help_text=u'Example of a Django DateField with a date picker',
+        auto_now=True,
+        null=True
+    )
+
+    datetime = models.DateTimeField(
+        u'Datetime',
+        help_text=u'Example of a Django DateTimeField with a datetime picker',
+        auto_now=True,
+        null=True
+    )
+
+    time = models.TimeField(
+        u'Time',
+        help_text=u'Example of a Django TimeField with a time picker',
+        auto_now=True,
+        null=True
+    )
+
+    start_date = models.DateField(
+        u'Start date',
+        help_text=u'Start date needs to be before end date.',
+        auto_now=True,
+        null=True
+    )
+
+    end_date = models.DateField(
+        u'End date',
+        help_text=u'End date needs to be after start date.',
+        auto_now=True,
+        null=True
+    )
+
+    attachment = models.FileField(
+        u'Attachment',
+        help_text=u'File attachment',
+        null=True,
+        blank=True
+    )
+
+    image = models.ImageField(
+        u'Image',
+        help_text=u'Image attachment',
+        null=True,
+        blank=True
+    )
+
+    url = models.URLField(
+        u'URL',
+        help_text=u'URL help text',
+        null=True,
+        blank=True
+    )
+
+    uuid = models.UUIDField(
+        u'UUID',
+        help_text=u'UUID help text',
+        null=True,
+        blank=True
+    )
+
+    gender = models.CharField(
+        u'Gender',
+        help_text=u'Gender help text',
+        max_length=6,
+        null=True,
+        blank=True,
+        choices=(('m', 'male'), ('f', 'female'),)
+    )
+
+    first_time_application = models.NullBooleanField(
+        u'First time application',
+        help_text=u'Is this your first time application?',
+    )
+
+    first_time_application_reason = models.CharField(
+        u'Reason for first time application',
+        null=True,
+        max_length=255
+    )
---
+++ djangorestframework-schemaform/rest_framework_schemaform/tests/test_serializers.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from rest_framework_schemaform.serializers import JsonSchemaSerializer
+from rest_framework_schemaform.models import Application
+
+import pytest
+
+
+@pytest.mark.django_db
+def test_json_schema_serializer():
+    app = Application()
+    app.title = u'My first application'
+    app.firstname = u'John'
+    app.lastname = u'Doe'
+    app.email = u'john@doe.com'
+    app.save()
+
+    serializer = JsonSchemaSerializer(app)
+    result = serializer.to_representation(app)
+
+    assert result.get('title').startswith('Application')
+    assert 'object' == result.get('type')
+
+    assert 'salutation' == result['properties']['salutation']['key']
+    assert 'salutation' == result['properties']['salutation']['title']
+    assert 'string' == result['properties']['salutation']['type']
+    assert 'salutation' in result['form']
+
+    assert 'firstname' == result['properties']['firstname']['key']
+    assert 'First name' == result['properties']['firstname']['title']
+    assert 'string' == result['properties']['firstname']['type']
+    assert 'firstname' in result['required']
+    assert 'firstname' in result['form']
+
+    assert 'lastname' == result['properties']['lastname']['key']
+    assert 'Last name' == result['properties']['lastname']['title']
+    assert 'string' == result['properties']['lastname']['type']
+    assert 'lastname' in result['form']
+
+    assert 'email' == result['properties']['email']['key']
+    assert 'Email address' == result['properties']['email']['title']
+    assert 'string' == result['properties']['email']['type']
+    assert '^\\S+@\\S+$' == result['properties']['email']['pattern']
+    assert 'email' in result['form']
+
+    assert 'first_time_application' == result['properties']['first_time_application']['key']  # noqa
+    assert 'first_time_application' == result['properties']['first_time_application']['title']  # noqa
+    assert 'boolean' == result['properties']['first_time_application']['type']
+    assert 'first_time_application' in result['form']
